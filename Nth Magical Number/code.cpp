08b1faab2015c34ec0bcf21811ebd6ed8d469835
JetavyaSingh <Jetavyasingh.JOY7@gmail.com>
2026-03-13 10:05:15 +0530

    Refactor nthMagicalNumber function for clarity

--- Nth Magical Number/code.cpp
+++ Nth Magical Number/code.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-
     typedef long long ll;
-    ll mod = 1e9 + 7;
+    const int mod = 1e9 + 7;
 
-    ll gcd(ll a, ll b) {
+    ll gcd(ll a, ll b){
         return b == 0 ? a : gcd(b, a % b);
     }
 
     int nthMagicalNumber(int n, int a, int b) {
-          ll L = (ll)a * b / gcd(a, b);
+        ll l = min(a,b);
+        ll r = (ll)n * min(a,b);
 
-        for(int count = 1;count<=n;count++){
-            if(i*a < j*b){
-                val = i*a;
-                i++;
-            }else if(j*b < i*a){
-                val = j*b;
-                j++;
-            }else{
-                val = i*a;
-                i++;
-                j++;
-            }
+        ll lcm = (ll)a * b / gcd(a,b);
+
+        while(l < r){
+            ll mid = l + (r-l)/2;
+
+            ll count = mid/a + mid/b - mid/lcm;
+
+            if(count < n)
+                l = mid + 1;
+            else
+                r = mid;
         }
-        return val % mod;
+
+        return l % mod;
     }
 };
